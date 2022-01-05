712284059cba6e76a7e02ece17c2e561bafacc25
Aristidis13 <aris13pat@hotmail.com>
2022-01-05 02:13:12 +0200

    Added 'data-' attribute

--- src/modules/Button/button.js
+++ src/modules/Button/button.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './button.module.css'
 
 export const Button = (props) => {
     return(
-        <button type="button" value={props.text} data-button-type={props.type}>
+        <button type="button" className={styles.button} value={props.text} data-button-type={props.type}>
             {props.text}
         </button>
     )
