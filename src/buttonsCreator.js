2b39a9f74aef0fb2eca55c74d1b47abb8d419800
Aristidis13 <aris13pat@hotmail.com>
2022-01-05 02:11:26 +0200

    Added listBtnItem

--- src/buttonsCreator.js
+++ src/buttonsCreator.js
@@ -3,7 +3,7 @@ import {Button} from './modules/Button/button.js'
 export const buttonsThatAreNumbers = createJSXSyntax( [...Array(10).keys()], "number" );
 
 export const buttonsThatAreOperations = createJSXSyntax(
-[ "del", "reset", "cos", "sin",  "r" ,   "e"  , "sqr", "x^y",
+[ "del", "tan", "cos", "sin",  "r" ,   "e"  , "sqr", "x^y",
    "+" ,   "-"  ,  "x" ,  "/" , "log",   "Π"  ,  "x!",  "%" ], "operation"
 );
 
@@ -12,5 +12,5 @@ export const buttonsThatAreResults = createJSXSyntax([ "reset", "=", "del"],"res
 /*Accepts array with buttons -
   Returns JSX Elements */
 function createJSXSyntax(ar,type) {
-    return ar.map( (button) => (<li key={button}><Button text ={button} data-btn-type={type} /></li> ));
+    return ar.map( (button) => (<li className="listBtnItem" key={button}><Button text ={button} data-btn-type={type} /></li> ));
 }
