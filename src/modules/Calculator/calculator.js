08b07ce65de4d1096004b108192d8ff6f7059e8a
Aristidis13 <aris13pat@hotmail.com>
2022-01-05 02:14:26 +0200

    Imported css module

--- src/modules/Calculator/calculator.js
+++ src/modules/Calculator/calculator.js
@@ -2,17 +2,17 @@ import {Screen} from '../Screen/screen.js';
 import {buttonsThatAreNumbers as nbtns, 
         buttonsThatAreOperations as obtns,
         buttonsThatAreResults as rbtns} from '../../buttonsCreator.js';
-
+import styles from './calculator.module.css';
 
 
 export const Calculator = () => {  
     return(
-        <section id="container">
-            <Screen input/>
-            <div id="buttons">
-                <ul id="number"> {nbtns} </ul>
-                <ul id="operations"> {obtns} </ul>
-                <ul id="results">{rbtns} </ul>
+        <section id={styles.calculator}>
+            <Screen input=""/>
+            <div className={styles.buttons}>
+                <ul id="numbers" className={styles.btnList}> {nbtns} </ul>
+                <ul id="operations" className={styles.btnList}> {obtns} </ul>
+                <ul id="results" className={styles.btnList}>{rbtns} </ul>
             </div>
         </section>
     )
