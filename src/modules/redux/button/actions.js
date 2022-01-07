4f21e7a629a71cf7729b9aeb93ba7aec87a5521c
Aristidis13 <aris13pat@hotmail.com>
2022-01-08 01:54:41 +0200

    Deleted useless files

--- src/modules/redux/button/actions.js
+++
@@ -1,8 +0,0 @@
-import {PRESS_BUTTON} from './types.js';
-
-export const pressBtn = () => {
-    return {
-        type: PRESS_BUTTON,
-        info: 'Executes when the user presses a button'
-    }
-}
--- src/modules/redux/button/types.js
+++
@@ -1,1 +0,0 @@
-export const PRESS_BUTTON = 'PRESS_BUTTON';
