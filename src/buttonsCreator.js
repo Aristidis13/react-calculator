705a783b14dbe86e269a9bf8be4648d0780772b8
Aristidis13 <aris13pat@hotmail.com>
2022-01-13 02:47:06 +0200

    Added dot and pi button

--- src/buttonsCreator.js
+++ src/buttonsCreator.js
@@ -1,16 +1,19 @@
 import Button from './modules/Button/button.js'
 
-export const parenthesizedArray = ["tan", "cos", "sin", "sinh", "log", "ln", "sqr"];
-export const oneDirectionalArray = [ "e",  "x!", "."];
-export const biDirectionalArray = [ "+", "-", "%", "x^y", "/", ];
+export const oneDirectionalArray = [ "tan", "cos", "sin", "sinh", "log", "ln", "sqr","e",  "x!"];
+export const biDirectionalArray = [ "+", "-", "%", "x^y", "/","*" ];
+export const numberArray = [...Array(10).keys()];
+export const dotArray = ['.'];
+export const PIArray = ['Π'];
 
-export const numbers = createJSXSyntax( [...Array(10).keys()], "number" );
+export const numbers = createJSXSyntax( numberArray, "number" );
 export const oneDirectional = createJSXSyntax(oneDirectionalArray, "onedirectional");
-export const parenthesized = createJSXSyntax(parenthesizedArray,"parenthesized");
 export const biDirectional = createJSXSyntax(biDirectionalArray,"bidirectional");
 export const reset = createJSXSyntax(["reset"],"reset");
 export const equal = createJSXSyntax([  "="  ],"equal");
 export const del = createJSXSyntax([ "del" ],"delete");
+export const dot = createJSXSyntax(dotArray,"dot"); 
+export const PI = createJSXSyntax(PIArray,"PI"); 
 
 /*Accepts array with buttons -
   Returns JSX Elements */
